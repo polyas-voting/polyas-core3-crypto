e4df53c5da6ab51c597ff29323616405140deecc
Tomasz Truderung <ttruderung@gmail.com>
2025-06-09 12:36:30 +0200

    add licence notes to the kt files

--- crypto-std/src/main/kotlin/de/polyas/core3/crypto/std/DeterministicKeyEncryption.kt
+++ crypto-std/src/main/kotlin/de/polyas/core3/crypto/std/DeterministicKeyEncryption.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2025 Polyas GmbH
+ *
+ * Licensed under GNU Affero General Public License v3.0; you may not use
+ * this file except in compliance with the License. You may obtain a copy of
+ * the License at https://www.gnu.org/licenses/agpl-3.0.en.html.
+ * This software is distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied.
+ */
 package de.polyas.core3.crypto.std
 
 import kotlin.experimental.xor
